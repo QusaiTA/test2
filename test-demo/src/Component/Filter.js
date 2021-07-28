f8cdb1dd68bd25b3be0d9a363a3f66303008739e
QusaiTA <tashtoshqusai@gmail.com>
2021-07-28 18:31:24 +0300

    not pushing

---
+++ test-demo/src/Component/Filter.js
@@ -0,0 +1,33 @@
+// import React from 'react';
+// import Form from 'react-bootstrap/Form';
+
+
+// class Filter extends React.Component{
+//    getThesubmittedData = (event) => {
+//      event.preventDefault();
+//      let gittedData = event.target.Select.value;
+//      this.props.submittedData(gittedData);
+//    }
+
+//    render(){
+
+
+//      return (
+
+//        <div>
+//          <Form onSubmit ={this.getThesubmittedData}>
+//            <Form.Select aria-label="Default select example">
+//              <option>Open this select menu</option>
+//              <option value="1">One</option>
+//              <option value="2">Two</option>
+//              <option value="3">Three</option>
+//              <option value="4">Whee!</option>
+
+//            </Form.Select>
+//          </Form>
+//        </div>
+//      );
+
+//    }}
+
+// export default Filter;
